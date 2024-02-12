79c4f156221d9ca80e8609e9f3d58efdc515e430
Aung Myo <aungmyo2123@gmail.com>
2024-02-12 15:25:57 +0630

    [2024-02-05] -> First LessonAdd project files.

--- AMDotNetTraningBatch3.ConsoleApp/Program.cs
+++ AMDotNetTraningBatch3.ConsoleApp/Program.cs
@@ -1,2 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
+using System.Data.SqlClient;
+using AMDotNetraningBatch3.ConsoleApp.AdoDotNet;
+using AMDotNetraningBatch3.ConsoleApp.Dapper;
+using AMDotNetraningBatch3.ConsoleApp.EFCore;
+
 Console.WriteLine("Hello, World!");
+
+// F5 => Run
+
+// Ctrl + K, C => Disable
+// Ctrl + K, C => Enable
+
+//Console.ReadLine();
+
+// Ctrl + .
+// F9 => Break point
+// Shift + F5 => Stop 
+
+// UI, BL, DA => SQL
+
+// CRUD
+
+// ntier three tier
+
+// Kpay
+// Tranfer
+// from mobile no
+// to mobile no
+// amount
+// passcode
+
+// mobile no (-1000)
+// mobile no (+1000)
+// from to +1000 date
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = ".";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sasa@123";
+
+Console.WriteLine("ConnectionString => " + sqlConnectionStringBuilder.ConnectionString);
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+Console.WriteLine("Connection Opening...");
+connection.Open();
+Console.WriteLine("Connection Opened...");
+
+//// data set
+//// data table
+//// data row
+//// data column
+
+string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+  FROM [dbo].[Tbl_Blog]";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+Console.WriteLine("Connection Closing...");
+connection.Close();
+Console.WriteLine("Connection Closed...");
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Title..." + dr["BlogTitle"]);
+    Console.WriteLine("Author..." + dr["BlogAuthor"]);
+    Console.WriteLine("Content..." + dr["BlogContent"]);
+}
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
+adoDotNetExample.Edit(1);
+adoDotNetExample.Edit(11);
+adoDotNetExample.Create("test title", "test author", "test content");
+adoDotNetExample.Update(1002, "test title 2", "test author 2", "test content 2");
+adoDotNetExample.Update(1002, content: "test title 2", author: "test author 2", title: "test content 2");
+adoDotNetExample.Delete(1002);
+
+DapperExample dapperExample = new DapperExample();
+dapperExample.Read();
+
+dapperExample.Edit(1);
+dapperExample.Edit(11);
+
+dapperExample.Create("test title", "test author", "test content");
+dapperExample.Update(2002, "test title 2", "test author 2", "test content 2");
+dapperExample.Delete(2002);
+
+EFCoreExample efCoreExample = new EFCoreExample();
+efCoreExample.Read();
+
+efCoreExample.Edit(1);
+efCoreExample.Edit(11);
+
+efCoreExample.Create("test title", "test author", "test content");
+efCoreExample.Update(3002, "test title 2", "test author 2", "test content 2");
+efCoreExample.Delete(3002);
+
+// hello
+
+Console.ReadKey();
